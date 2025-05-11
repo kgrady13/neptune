b6d2b9227bd3292e7f5952838d7b8cd51336a816
kgrady13 <karson@massivelyhuman.com>
2025-05-11 15:28:49 -0700

    max duration set to 5 sec

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,8 @@
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
+export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+
+
 module.exports = createRequestHandler({
   build: require('path').join(__dirname, '../dist/server'),
 });
